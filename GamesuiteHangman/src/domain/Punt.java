74c6e3fad2b966a6cbad0c758b35e06df4ba5e04
Rottert <robbert_hendrickx@hotmail.com>
2017-05-09 12:30:44 +0200

    Doener: Robbert Denker: Robbert Story: Tests Aanmaken en verbeteren van klassen

--- GamesuiteHangman/src/domain/Punt.java
+++ GamesuiteHangman/src/domain/Punt.java
@@ -2,4 +2,8 @@ package domain;
 
 public class Punt {
 
+	public Punt(int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
 }
---
+++ GamesuiteHangman/test/domain/Driehoek.java
@@ -0,0 +1,24 @@
+package domain;
+
+public class Driehoek {
+
+	public Driehoek(Punt punt1, Punt punt2, Punt punt3) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object getHoekPunt1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getHoekPunt2() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getHoekPunt3() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
