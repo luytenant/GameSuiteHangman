ae57d49a405a48e421892c2b58badbd9b0bf2348
randomgitt <vince.lukkesen@student.ucll.be>
2017-05-11 14:09:49 +0200

    Doener Vince Denker Pieter-Jan + Jonas US 8 Status: afgewerkt

--- GamesuiteHangman/src/domain/Tekening.java
+++ GamesuiteHangman/src/domain/Tekening.java
@@ -10,34 +10,67 @@ public class Tekening {
 	private static final int MAX_X = 399;
 	private static final int MIN_Y = 0;
 	private static final int MAX_Y = 399;
-	
-	public Tekening(String naam){
-		
+
+	public Tekening(String naam) {
+
 		setNaam(naam);
 		this.tekening = new ArrayList<Vorm>();
 	}
-	
+
 	private void setNaam(String naam) {
 		if (naam == null || naam.trim().isEmpty()) {
-			throw new DomainException("Naam mag niet leeg zijn!");
+			throw new IllegalArgumentException("Naam mag niet leeg zijn!");
 		} else {
 			this.naam = naam;
 		}
 	}
-	
-	public String getNaam(){
+
+	public String getNaam() {
 		return this.naam;
 	}
-	
-	public void voegToe(Vorm vorm){
+
+	public void voegToe(Vorm vorm) {
 		tekening.add(vorm);
 	}
-	public int getAantalVormen(){
+
+	public int getAantalVormen() {
 		return tekening.size();
 	}
-	public Vorm getVorm(int index){
+
+	public Vorm getVorm(int index) {
 		return tekening.get(index);
 	}
-	
-	
+
+	public boolean equals(Object object) {
+		boolean result = false;
+		if (object instanceof Tekening) {
+			Tekening p = (Tekening) object;
+			if (p.getAantalVormen() == this.getAantalVormen()) {
+				result = true;
+			}
+			int counter = 0;
+			for (int i = 0; i < tekening.size(); i++) {
+				for (int j = 0; j < p.tekening.size(); j++) {
+					if (p.getVorm(j) == tekening.get(i)) {
+						counter++;
+					}
+				}
+			}
+			if (counter == tekening.size() && counter == p.tekening.size()) {
+				result = true;
+			}else {
+				result = false;
+			}
+		}
+		return result;
+	}
+
+	public void verwijder(Vorm vorm) {
+		tekening.remove(vorm);
+	}
+
+	public boolean bevat(Vorm vorm) {
+		return tekening.contains(vorm);
+	}
+
 }
